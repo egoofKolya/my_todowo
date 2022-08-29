72ea96d9dfe8863336f0b6fe0245c801925d5ff3
Kolya Dubyna <mykola22dubyna@gmail.com>
2022-08-29 15:20:17 +0300

    Norm todo

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk-ua'
 
-TIME_ZONE = 'Etc/GMT-3'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
